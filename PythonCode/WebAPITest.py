ec730e1c984856919b460f2bc0d962dc1ef1c86a
Aaron Ogle <156380776+Aogle2@users.noreply.github.com>
2024-04-18 18:10:02 -0400

    adding some error correction.

--- PythonCode/WebAPITest.py
+++ PythonCode/WebAPITest.py
@@ -31,6 +31,9 @@ def NewResponder():
         cl,addr = s.accept()
         request_data = cl.recv(1024).split()
         request = request_data[1] if len(request_data) > 1 else "No Request"
+
+        if request == "b'/'":
+            print("This is a test")
         
         cl.send('HTTP/1.0 200 OK\r\nContent-type: application/json\r\n\r\n'.encode())
         print(f"Client Connected from: {addr} with request: {request}\nResponded: {response} to client")
