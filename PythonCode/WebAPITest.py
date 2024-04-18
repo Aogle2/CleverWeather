a2d215eef6b496f763a3e657409ea2dc61fd74e2
Aaron Ogle <156380776+Aogle2@users.noreply.github.com>
2024-04-18 18:38:54 -0400

    remove some data

--- PythonCode/WebAPITest.py
+++ PythonCode/WebAPITest.py
@@ -31,9 +31,6 @@ def NewResponder():
         cl,addr = s.accept()
         request_data = cl.recv(1024).split()
         request = request_data[1] if len(request_data) > 1 else "No Request"
-
-        if request.decode() == "/":
-            print("This is the default.")
         
         cl.send('HTTP/1.0 200 OK\r\nContent-type: application/json\r\n\r\n'.encode())
         print(f"Client Connected from: {addr} with request: {request}\nResponded: {response} to client")
