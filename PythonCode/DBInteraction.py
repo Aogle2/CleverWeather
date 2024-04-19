8aeb04baaac1d650dc1833a9b41d6ab9140bb143
Aaron Ogle <156380776+Aogle2@users.noreply.github.com>
2024-04-19 15:35:13 -0400

    Exploration

--- PythonCode/DBInteraction.py
+++ PythonCode/DBInteraction.py
@@ -3,8 +3,15 @@ from mysql import connector as cn
 
 
 def DBConnect():
-    connection = cn.connect(user ="aogle", password = "local#123", host="raspberrypi")
+    connection = cn.connect(user ="aogle", password = "local#123", host="raspberrypi",database='mysql')
     print(connection.get_server_info())
+    cur = connection.cursor()
+    querey = "SELECT * FROM user;"
+    cur.execute(querey)
+    for row in cur.fetchall():
+        print(row)
+        print(row[0])
+
     connection.close()
 
 
