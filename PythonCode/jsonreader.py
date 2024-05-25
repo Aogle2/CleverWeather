e042391aceea137defbd980c751d30b13b9d0dd3
Aaron Ogle <aaronogle7@gmail.com>
2024-05-24 23:05:51 -0400

    Creating and reading the JSON file..or an attempt at it.

---
+++ PythonCode/jsonreader.py
@@ -0,0 +1,7 @@
+import json
+import os
+
+print(os.path.join(os.getcwd(),"Configuration","BaseDBConfiguration.json"))
+
+#with (os.path.join(os.getcwd(),"Configuration","BaseDBConfiguration.json")) as jsonfile:
+#    print(jsonfile.read())
