2b705deafb7ab2ac3b393b766955e93b330a91ea
Aaron Ogle <aaronogle7@gmail.com>
2024-05-24 22:36:19 -0400

    Getting a repones from the python webserver.

--- PythonCode/WebAPIPull.py
+++ PythonCode/WebAPIPull.py
@@ -1,6 +1,11 @@
 import requests as req
+import json
 
 r = req.get("http://localhost:8088")
 
-print(r.json()["machine_arc"])
+print(r.json()["machine_arc"][0])
+
+for x,v in r.json().items():
+    print(f"{x}:{v}")
+
 
