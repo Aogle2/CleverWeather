a7a9aa0f1e1dccc3940e12d8fc06596bddbd0cf0
Aaron Ogle <aaronogle7@gmail.com>
2024-05-29 16:29:29 -0400

    Cleanup

--- PythonCode/WebAPIPull.py
+++ PythonCode/WebAPIPull.py
@@ -1,5 +1,6 @@
 import requests as req
 import json
+import os
 
 r = req.get("http://localhost:8088")
 
@@ -7,7 +8,6 @@ print(r.json()["machine_arc"][0])
 
 for x,v in r.json().items():
     print(f"{x}:{v}")
-
 
 if "machine_arc" in r.json():
     print("This has the correct key")
