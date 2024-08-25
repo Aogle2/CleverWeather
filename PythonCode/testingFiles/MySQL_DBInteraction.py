199fdaebfbc564498b8564a1604013d93b4cb5da
Aaron Ogle <aaronogle7@gmail.com>
2024-08-24 23:51:58 -0400

    Re-orgianization

---
+++ PythonCode/testingFiles/MySQL_DBInteraction.py
@@ -0,0 +1,24 @@
+from mysql import connector as cn
+
+
+
+def DBConnect():
+    connection = cn.connect(user ="root", password = "local#123", host="localhost",database='information_schema')
+    print(connection.get_server_info())
+    cur = connection.cursor()
+    querey = "show tables;"
+    cur.execute(querey)
+    for row in cur.fetchall():
+        #print(row[0])
+        for table in row:
+            new  = connection.cursor()
+            newq = f"select * from {table};"
+            new.execute(newq)
+            for newstuff in new.fetchall():
+                print(newstuff)
+
+    cur.close()
+    connection.close()
+
+
+DBConnect()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CleverWeather
 Used for a home project to make a clever home weather system
+yes, I know...there are more than one out there, home assistance and what not.
+but I just want to expand my Python skills my way.
