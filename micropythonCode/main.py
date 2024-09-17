19f41d4764fcb8247043eea8b6850a56920e75fd
Aaron Ogle <aaronogle7@gmail.com>
2024-09-17 02:06:40 -0400

    starting to try to do the cool things.

--- micropythonCode/main.py
+++ micropythonCode/main.py
@@ -2,7 +2,7 @@ from netconnection import *
 from sensors import *
 
 
-Connection("airCube-79E","local#123")#.ConnectNow()
+Connection("airCube-79E","local#123").ConnectNow()
 
 print(TempHumiditySensor(2).ReadSensorOverall())
 
--- micropythonCode/netconnection.py
+++ micropythonCode/netconnection.py
@@ -1,24 +1,31 @@
 import network as nw
 import machine
+from time import sleep
 
 
 class Connection():
     def __init__(self, ssid, password):
         self.wifiName = ssid
         self.wifipass = password
+        self.OnboardLED = machine.Pin("LED", machine.Pin.OUT)
         print(f"{self.wifiName} will be connected to...")
+
+    def ToggleLED(self):
+        self.OnboardLED.toggle()
 
     def ConnectNow(self):
         wlan = nw.WLAN(nw.STA_IF)
         wlan.active(True)
         wlan.connect(self.wifiName, self.wifipass)
 
-        while wlan.isconnected():
+        if wlan.status() == 1:
+            print(f"Incorrect Password for {self.wifiName}")
 
+        if wlan.isconnected():
+            self.ToggleLED()
 
+# implement part of this here: https://programmingdigest.com/getting-started-with-raspberry-pi-pico-w-built-in-wifi/#:~:text=If%20the%20Pico%20W%20is%20not%20connected%20to,status%20messages%2C%20will%20be%20displayed%20on%20the%20console.
 
-
-#implement part of this here: https://programmingdigest.com/getting-started-with-raspberry-pi-pico-w-built-in-wifi/#:~:text=If%20the%20Pico%20W%20is%20not%20connected%20to,status%20messages%2C%20will%20be%20displayed%20on%20the%20console.
 
 
 
